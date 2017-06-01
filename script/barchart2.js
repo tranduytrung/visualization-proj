87a3e8d0172b4a0d040ae761902af98dfec07921
Ha Truong <thuha0921@gmail.com>
2017-06-01 14:57:51 +0700

    rearrange code in barchart2.js

--- script/barchart2.js
+++ script/barchart2.js
@@ -5,40 +5,16 @@ var height = 500;
 // Parse the date / time
 var	parseDate = d3.time.format("%Y-%m").parse;
 
-// // using tab-seperated values TSV
-// var data
-// d3.csv("input/data.barchart.tsv", function(error, tsvdata) {
-
-
 // using CSV file
-// because downloading is asynchronous, chart definition in put in the callback function of downloading file 
+// because downloading is asynchronous, chart definition in put in the callback function of downloading file
 var data = d3.csv("input/data.barchart.csv", function(error, data) {
 	
     data.forEach(function(d) {
         d.date = parseDate(d.date);
         d.value = +d.value;
     });
-    console.log(data)
 
-	var x = d3.scale.ordinal()
-					.domain(data.map(function(d) { return d.date; }))
-					.rangeRoundBands([0, width], 0.1);
-
-	var y = d3.scale.linear()
-					.range([height, 0])
-					.domain([0, d3.max(data, function(d) { return d.value; })]);
-
-	var xAxis = d3.svg.axis()
-	    .scale(x)
-	    .orient("bottom")
-	    .tickFormat(d3.time.format("%Y-%m"));
-
-	var yAxis = d3.svg.axis()
-	    .scale(y)
-	    .orient("left")
-	    .ticks(10);
-
-	var svg = d3.select('body')
+    var svg = d3.select('body')
 		.append('svg')
 		.attr("width", width + margin.right + margin.left)
 		.attr("height", height + margin.top + margin.bottom);
@@ -46,13 +22,17 @@ var data = d3.csv("input/data.barchart.csv", function(error, data) {
 	// g: group all elements
 	var g = svg.append("g")
 				// move from (0,0) to (margin.left, margin.top)
-				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+				.attr("transform", 
+					"translate(" + margin.left + "," + margin.top + ")");
 
-	// group: y Axis
-	g.append("g")
-		// apply css .axis to "y Axis"
-		.attr("class", "y axis")
-		.call(yAxis);
+	var x = d3.scale.ordinal()
+					.domain(data.map(function(d) { return d.date; }))
+					.rangeRoundBands([0, width], 0.1);
+
+	var xAxis = d3.svg.axis()
+	    .scale(x)
+	    .orient("bottom")
+	    .tickFormat(d3.time.format("%Y-%m"));
 
 	// group: x Axis
 	g.append("g")
@@ -66,6 +46,21 @@ var data = d3.csv("input/data.barchart.csv", function(error, data) {
 		.style("text-anchor", "end")
 		.attr("transform", "rotate(-90)" );
 
+	var y = d3.scale.linear()
+					.range([height, 0])
+					.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+	var yAxis = d3.svg.axis()
+	    .scale(y)
+	    .orient("left")
+	    .ticks(10);
+
+	// group: y Axis
+	g.append("g")
+		// apply css .axis to "y Axis"
+		.attr("class", "y axis")
+		.call(yAxis);
+
 	// draw bar chart
 	g.selectAll('bar')
 		.data(data)
@@ -77,3 +72,8 @@ var data = d3.csv("input/data.barchart.csv", function(error, data) {
 		.attr("y", function(d) {return y(d.value)})
 		.attr("height", function(d) { return height - y(d.value); });
 });
+
+
+// // using tab-seperated values TSV
+// var data
+// d3.csv("input/data.barchart.tsv", function(error, tsvdata) {
