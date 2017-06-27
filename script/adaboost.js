19b81be4e78b0d3d6a2de5f344427422eb162602
Ha Truong <thuha0921@gmail.com>
2017-06-27 17:53:49 +0700

    implement load file, adaboost algorithm

--- script/adaboost.js
+++ script/adaboost.js
@@ -1,7 +1,7 @@
 // custom variables
 var margin = {top: 20, right: 20, bottom: 60, left: 30};
-var width = 600;
-var height = 600;
+var width = Math.floor($('#adaboost_graph').width()/100) * 100;
+var height = width;
 var speed = 1000;
 var iterators;
 var states = {"initial": 0, "start": 1, "running": 2, "stop": 3, "finished": 4};
@@ -9,15 +9,20 @@ var current_state = states.initial;
 var current_iterator = -1;
 var timer;
 var svg;
+var g;
 
 var max_x_axis = 10;
 var max_y_axis = 10;
 
 var color_point = {"blue": "#1f77b4", "red": "#ff7f0e"}
 var color_region = {"blue": "#aec7e8", "red": "#ffbb78"}
-	 
-var rScale = d3.scale.linear().domain([0,width]).range([0,20]);
-var rCircle = 200;
+
+// Scale
+var xScale = d3.scale.linear().range([0, width]).domain([0, max_x_axis]);
+var yScale = d3.scale.linear().range([height, 0]).domain([0, max_y_axis]);
+var rScale = d3.scale.linear().range([0, width]).domain([0, 20]);
+var rCircle = 0.25;
+
 var o = d3.scale.linear().domain([10000,100000]).range([.5,1]);
 
 var lineId = 'regline';
@@ -52,15 +57,9 @@ function initialGraph() {
             .attr("height", height + margin.top + margin.bottom);
 
     // g: group all elements
-    var g = svg.append("g")
-                // move from (0,0) to (margin.left, margin.top)
-                .attr("transform", 
-                    "translate(" + margin.left + "," + margin.top + ")");
-
-    // xAxis
-    var xScale = d3.scale.linear()
-                .range([0, width])
-                .domain([0, max_x_axis]);
+    g = svg.append("g")
+			// move from (0,0) to (margin.left, margin.top)
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     var xAxis = d3.svg.axis()
         .scale(xScale)
@@ -74,10 +73,6 @@ function initialGraph() {
         .call(xAxis);
 
     // yAxis
-    var yScale = d3.scale.linear()
-                .range([height, 0])
-                .domain([0, max_y_axis]);
-
     var yAxis = d3.svg.axis()
         .scale(yScale)
         .orient("left");
@@ -89,26 +84,26 @@ function initialGraph() {
         .call(yAxis);
 	
 	//draw dashed lines
-	g.selectAll(".h").data(d3.range(0,10,1)).enter()
+	g.selectAll(".h").data(d3.range(0, max_y_axis, 1)).enter()
 	  .append("line").classed("h",1)
 	  .attr("x1", 0).attr("x2",height)
 	  .attr("y1", yScale).attr("y2", yScale)
 	  
-	g.selectAll(".v").data(d3.range(0,10,1)).enter()
+	g.selectAll(".v").data(d3.range(0, max_x_axis,1)).enter()
 	  .append("line").classed("v",1)
 	  .attr("y1", 0).attr("y2",width)
 	  .attr("x1", xScale).attr("x2", xScale)
 	
 	svg.on('contextmenu', function() {
 		//handle right click
-		drawCircle(d3.mouse(this)[0], d3.mouse(this)[1], color_point.red);
+		checkCirclePosition(d3.mouse(this)[0], d3.mouse(this)[1], color_point.red);
 		//stop showing browser menu
 		d3.event.preventDefault();
 	});
 
 	//click event: draw blue circle
 	svg.on('click', function(){
-		drawCircle(d3.mouse(this)[0], d3.mouse(this)[1], color_point.blue);
+		checkCirclePosition(d3.mouse(this)[0], d3.mouse(this)[1], color_point.blue);
 	});
 	
 }
@@ -176,46 +171,52 @@ function enableButtons(){
 	$('#speed').prop('disabled', !buttonStates.speed);
 }
 
-function drawCircle(mouse_x, mouse_y, color){
+function checkCirclePosition(mouse_x, mouse_y, color) {
 	if(mouse_x >= (margin.left + rScale(rCircle)) 
 		&& mouse_x <= (margin.left + width - rScale(rCircle)) 
 		&& mouse_y >= (margin.top + rScale(rCircle)) 
 		&& mouse_y <= (margin.top + height - rScale(rCircle))){
 		//push new data point to data array
-		data.push({"id": "point_" + (data.length + 1), "x": mouse_x, "y": mouse_y, "radius": rCircle, "fill": color, "opacity": 90000});
-
-		//select each circle and append the data
-		var selection = svg.selectAll("circle").data(data)
-
-		//update selection and draw new circle
-		selection.enter()
-		.append("circle")
-		.attr("id",function(d) {return d.id;})
-		.attr("cx",function(d) {return d.x;})
-		.attr("cy",function(d) {return d.y;})
-		.attr("r",function(d) {return rScale(d.radius);})
-		.style("fill",function(d) {return d.fill;})
-		.style("opacity",function(d) { return o(+d.opacity);});
-
-		//exit selection
-		selection.exit().remove()
-
-		if (data.length > 2) {
-			current_state = states.start;
-		}
-		enableButtons();
+		x = xScale.invert(mouse_x - margin.left);
+		y = yScale.invert(mouse_y - margin.top);
+		data.push({"id": "point_" + (data.length + 1), "x": x, "y": y, "radius": rCircle, "fill": color, "opacity": 90000});
+		
+		drawCircles();
 	}
 }
 
+function drawCircles(){
+	//select each circle and append the data
+	var selection = g.selectAll("circle").data(data)
+
+	//update selection and draw new circle
+	selection.enter()
+	.append("circle")
+	.attr("id",function(d) {return d.id;})
+	.attr("cx",function(d) {return xScale(d.x);})
+	.attr("cy",function(d) {return yScale(d.y);})
+	.attr("r",function(d) {return rScale(d.radius);})
+	.style("fill",function(d) {return d.fill;})
+	.style("opacity",function(d) { return o(+d.opacity);});
+
+	//exit selection
+	selection.exit().remove()
+
+	if (data.length > 2) {
+		current_state = states.start;
+	}
+	enableButtons();
+}
+
 function transition() {
-    svg.selectAll("circle")
+    g.selectAll("circle")
         .data(data)
         .transition()
         .duration(1500)
         .style("fill",function(d) {return d.fill;})
         .attr("r",function(d) {return rScale(d.radius);})
-        .attr("cx",function(d) {return d.x;})
-        .attr("cy",function(d) {return d.y;});
+        .attr("cx",function(d) {return xScale(d.x);})
+        .attr("cy",function(d) {return yScale(d.y);});
 	
 	if (current_iterator == 0){
 		drawline(iterators[current_iterator].line);
@@ -223,33 +224,28 @@ function transition() {
 		transitionline(iterators[current_iterator].line);
 	}
 }
+var lineFunction = d3.svg.line()
+		.x(function(d) { return xScale(d.x); })
+		.y(function(d) { return yScale(d.y); });
 
 var transitionline = function(lsCoef){
 	var a = lsCoef.a;
 	var b = lsCoef.b;
-	
-	var lineFunction = d3.svg.line()
-		.x(function(d) { return d.x; })
-		.y(function(d) { return d.y; });
 
 	d3.select('#' + lineId)
 		.transition()
 		.duration(1500)
-		.attr("d", lineFunction([{"x": margin.left, "y": a * margin.left + b},
-								{"x": (margin.left + width), "y": a * (margin.left + width) + b}]));
+		.attr("d", lineFunction([{"x": 0, "y": b},
+								{"x": max_x_axis, "y": a * max_x_axis + b}]));
 }
 
 function drawline(lsCoef){
 	var a = lsCoef.a;
 	var b = lsCoef.b;
 	
-	var lineFunction = d3.svg.line()
-		.x(function(d) { return d.x; })
-	.y(function(d) { return d.y; });
-
-	svg.append('path')
-		.attr("d", lineFunction([{"x": margin.left, "y": a * margin.left + b},
-								{"x": (margin.left + width), "y": a * (margin.left + width) + b}]))
+	g.append('path')
+		.attr("d", lineFunction([{"x": 0, "y": b},
+								{"x": max_x_axis, "y": a * max_x_axis + b}]))
 		.attr("stroke-width", 2)
 		.attr("stroke", "black")
 		.attr('id', lineId);
@@ -258,6 +254,7 @@ function drawline(lsCoef){
 function displayResult() {    
     timer = setInterval(function() {
         if (current_iterator < iterators.length) {
+			//TODO: scale radius: 0.25 + 0.75 * (x - min_weight)/(max_weight - min_weight)
 			// update data
 			$.each(data, function(i, item) {
 				data[i].radius = data[i].radius + rCircle * iterators[current_iterator].size_of_points[i] * 2;
@@ -282,27 +279,7 @@ document.getElementById("btnPlay").addEventListener("click", function() {
 	enableButtons();
 	
 	// run adaboost
-	iterators =  [
-        {
-            "iterator": 1,
-            "size_of_points": [0.1, 0.5, 0.2, 0, 0, 0, 0, 0.2],
-			"line": { "a": 0.1, "b": 1}
-		},
-		{
-            "iterator": 2,
-            "size_of_points": [0.3, 0.4, 0, 0, 0, 0, 0, 0.3],
-			"line": { "a": 0.5, "b": 2}
-		},
-		{
-            "iterator": 3,
-            "size_of_points": [0, 0, 0, 0, 0.5, 0.2, 0.2, 0.1],
-			"line": { "a": 1, "b": 3}
-		},
-		{
-            "iterator": 4,
-            "size_of_points": [0, 0, 0, 0.1, 0.3, 0.5, 0.1, 0],
-			"line": { "a": 2, "b": 4}
-		}];
+	iterators = adaboost_func(data);
 		
     clearInterval(timer);
     speed = $("#speed").val();
@@ -324,7 +301,7 @@ document.getElementById("btnReset").addEventListener("click", function() {
 	// remove line
 	$('#' + lineId).remove();
 
-	transition();	
+	drawCircles();
 });
 
 // stop
@@ -344,3 +321,160 @@ document.getElementById("btnClear").addEventListener("click", function() {
     initialGraph();
 });
 
+$("#btnLoadFile").on('click', function () {
+    var file = document.getElementById("inputFile").files[0];
+	if (file) {
+		var reader = new FileReader();
+		reader.readAsText(file, "UTF-8");
+		reader.onload = function (evt) {
+			//$("#errorLoadingFile").html(evt.target.result);
+			parseFile(evt.target.result, file.name);
+			reset();
+		}
+		reader.onerror = function (evt) {
+			$("#errorLoadingFile").html("Error reading file");
+		}
+	}
+})
+
+function parseFile(fileContent, fileName){
+	file_ext = fileName.substr(fileName.lastIndexOf('.') + 1);
+	data = [];
+	switch(file_ext){
+		case 'json':
+			json_data = JSON.parse(fileContent);
+			$.each(json_data, function(i) {
+				if (json_data[i].label == "blue" || json_data[i].label == 1){
+					color = color_point.blue;
+				} else {
+					color = color_point.red;
+				}
+				data.push({"id": "point_" + (i + 1), "x": json_data[i].x, "y": json_data[i].y, "radius": rCircle, "fill": color});
+			})
+			break;
+		case 'csv':
+		case 'txt':
+			break;
+		default:			
+	}
+}
+
+function reset(){
+	current_iterator = -1;
+	current_state = states.start;
+	enableButtons();
+	clearInterval(timer);
+	
+	// update data
+	$.each(data, function(i, item) {
+		data[i].radius = rCircle;
+	})
+	
+	initialGraph();
+
+	drawCircles();
+}
+
+$("#btnGenerateData").on('click', function () {
+	var nbPoints = $('#txtNumberPoint').val();
+});
+
+function adaboost_func(dataset) {
+	// convert label & initial weight
+	weights = [];
+	$.each(dataset, function(i){
+		dataset[i].label = dataset[i].fill == color_point.blue ? -1 : 1;
+		weights.push(1/dataset.length);
+	})
+	
+	iterators = [];
+	
+	for(i = 0; i < 5; i++){
+		// create simple classifier
+		classifier = simple_classifier(dataset, weights);
+		
+		iterators.push({"iterator": (i+1),
+            "size_of_points": weights,
+			"line": { "a": classifier.a, "b": classifier.b}});
+			
+		// compute weight of the classifier
+		alpha = 0.5 * Math.log((1 - classifier.error)/classifier.error);
+		
+		// update sample weights
+		new_weights = [];
+		sum_new_weight = 0;
+		$.each(dataset, function(j){
+			new_weights.push(weights[j] * Math.exp(-classifier.predicted_labels[j] * dataset[j].label * alpha));
+			sum_new_weight += new_weights[j];
+		})
+		$.each(new_weights, function(j){
+			new_weights[j] = new_weights[j]/sum_new_weight;
+		})
+		weights = new_weights;
+	};
+	
+	return iterators;
+}
+
+function simple_classifier(dataset, weights){
+	classiferResult = {"a": 0, "b": 0, "predicted_labels": [], "error": 0};
+	mean_group_1  = {"total_weight": 0, "sum_x": 0, "sum_y": 0, "x": 0, "y": 0};
+	mean_group_2 = {"total_weight": 0, "sum_x": 0, "sum_y": 0, "x": 0, "y": 0};
+	// compute mean of groups
+	$.each(dataset, function(i){
+		if (dataset[i].label == -1) {
+			mean_group_1.total_weight += weights[i];
+			mean_group_1.sum_x += dataset[i].x * weights[i];
+			mean_group_1.sum_y += dataset[i].y * weights[i];
+		} else {
+			mean_group_2.total_weight += weights[i];
+			mean_group_2.sum_x += dataset[i].x * weights[i];
+			mean_group_2.sum_y += dataset[i].y * weights[i];
+		}
+	})
+	
+	mean_group_1.x = mean_group_1.sum_x/mean_group_1.total_weight;
+	mean_group_1.y = mean_group_1.sum_y/mean_group_1.total_weight;
+	mean_group_2.x = mean_group_2.sum_x/mean_group_2.total_weight;
+	mean_group_2.y = mean_group_2.sum_y/mean_group_2.total_weight;
+	
+	// compute classification line	
+	mean_point = {"x": (mean_group_1.x + mean_group_2.x)/2, "y": (mean_group_1.y + mean_group_2.y)/2};
+	mm1_vector = {"x": (mean_point.x + (mean_group_1.y - mean_group_2.y)), "y": (mean_point.y + (mean_group_2.x - mean_group_1.x))};
+	
+	classiferResult.a = (mm1_vector.y - mean_point.y)/(mm1_vector.x - mean_point.x);
+	classiferResult.b = -mean_point.x * (mm1_vector.y - mean_point.y)/(mm1_vector.x - mean_point.x) + mean_point.y;
+	
+	// we use temporary labels: -2 for points which have a*x + b - y < 0 and 2 for ones which have a*x + b - y > 0 
+	count_predicted_labels = [[0,0],[0,0]];
+	$.each(dataset, function(i){
+		if (dataset[i].x * classiferResult.a + classiferResult.b - dataset[i].y < 0){
+			classiferResult.predicted_labels.push(-2);
+			if (dataset[i].label == -1)
+				count_predicted_labels[0][0] += 1;
+			else
+				count_predicted_labels[0][1] += 1;
+		} else {
+			classiferResult.predicted_labels.push(2);
+			if (dataset[i].label == -1)
+				count_predicted_labels[1][0] += 1;
+			else
+				count_predicted_labels[1][1] += 1;
+		}
+	})
+	
+	// update true label
+	true_label_1 = count_predicted_labels[0][0] > count_predicted_labels[0][1] ? -1 : 1;
+	$.each(classiferResult.predicted_labels, function(i){
+		if (classiferResult.predicted_labels[i] == -2){
+			classiferResult.predicted_labels[i] = true_label_1;
+		} else {
+			classiferResult.predicted_labels[i] = true_label_1 * -1;
+		}
+		classiferResult.error += weights[i] * classiferResult.predicted_labels[i] * dataset[i].label;		
+	})
+	
+	classiferResult.error = 0.5 - 0.5 * classiferResult.error;
+	
+	return classiferResult;
+}
